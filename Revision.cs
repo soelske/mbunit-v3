20b3a0ab5fac93538beb4415b77bc02fa793645d
Bart Suelze <bartsuelze@hotmail.com>
2020-03-29 13:12:08 +0200

    Revision. MtToolPath. CorflagsToolPath.

---
+++ Revision.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("1.67.4.0")]
+[assembly: AssemblyFileVersion("1.67.4.1")]
+
+[assembly: AssemblyCompany(xx_gener_dotnet.xx_Revision.xx_AssemblyCompany)]
+//[assembly: AssemblyProduct("TcLayoutPlan.Net")]
+[assembly: AssemblyCopyright(xx_gener_dotnet.xx_Revision.xx_AssemblyCopyrightBegin + "2007" + xx_gener_dotnet.xx_Revision.xx_AssemblyCopyrightEnd)]
+[assembly: AssemblyTrademark(xx_gener_dotnet.xx_Revision.xx_AssemblyTrademark)]

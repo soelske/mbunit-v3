f17ad937a46f2db83720e41e6a7140cbed6938ba
Bart Suelze <bartsuelze@hotmail.com>
2020-03-30 11:50:22 +0200

    Revision 4.0.0.0.

--- Revision.cs
+++ Revision.cs
@@ -1,10 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.67.4.0")]
-[assembly: AssemblyFileVersion("1.67.4.1")]
-
-[assembly: AssemblyCompany(xx_gener_dotnet.xx_Revision.xx_AssemblyCompany)]
-//[assembly: AssemblyProduct("TcLayoutPlan.Net")]
-[assembly: AssemblyCopyright(xx_gener_dotnet.xx_Revision.xx_AssemblyCopyrightBegin + "2007" + xx_gener_dotnet.xx_Revision.xx_AssemblyCopyrightEnd)]
-[assembly: AssemblyTrademark(xx_gener_dotnet.xx_Revision.xx_AssemblyTrademark)]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]

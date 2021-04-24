ebc6ef1d5bc2196b7583582250b4b14b28c8e45a
Bart <bart@i-theses.com>
2021-04-24 13:17:04 +0200

    assert excel are equal.

---
+++ src/MbUnit/MbUnit.Tests/Framework/AssertTest_Excel_AreEqual.cs
@@ -0,0 +1,29 @@
+﻿using MbUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+
+namespace MbUnit.Tests.Framework
+{
+  [TestsOn(typeof(Assert))]
+  public class AssertTest_Excel_AreEqual : BaseAssertTest
+  {
+    [Test]
+    [Row("..\\Framework\\AssertTest_Excel_AreEqual.xls", "..\\Framework\\AssertTest_Excel_AreEqual_copy.xls")]
+    public void AreEqual_passes(string expectedFileName, string actualFileName)
+    {
+      string expected = Path.Combine(Path.GetDirectoryName(typeof(AssertTest_Excel_AreEqual).Assembly.Location), expectedFileName);
+      string actual = Path.Combine(Path.GetDirectoryName(typeof(AssertTest_Excel_AreEqual).Assembly.Location), actualFileName);
+      Assert.Excel.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [Row("..\\Framework\\AssertTest_Excel_AreEqual.xls", "..\\Framework\\AssertTest_Excel_AreNotEqual.xls")]
+    [Row("..\\Framework\\AssertTest_Excel_AreEqual.xls", "..\\Framework\\AssertTest_Excel_AreNotEqual_missing_image.xls")]
+    public void AreNotEqual_passes(string expectedFileName, string actualFileName)
+    {
+      string expected = Path.Combine(Path.GetDirectoryName(typeof(AssertTest_Excel_AreEqual).Assembly.Location), expectedFileName);
+      string actual = Path.Combine(Path.GetDirectoryName(typeof(AssertTest_Excel_AreEqual).Assembly.Location), actualFileName);
+      Assert.Excel.AreNotEqual(expected, actual);
+    }
+  }
+}
---
+++ src/MbUnit/MbUnit/Framework/Assert.Excel.cs
@@ -0,0 +1,82 @@
+﻿using Gallio.Framework.Assertions;
+using System;
+
+namespace MbUnit.Framework
+{
+  public abstract partial class Assert
+  {
+    /// <summary>
+    /// Assertions for Excel data.
+    /// </summary>
+    public abstract class Excel
+    {
+      private static byte[] FileToByteArray(string fileName)
+      {
+        byte[] fileContent = null;
+        System.IO.FileStream fs = new System.IO.FileStream(fileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+        System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs);
+        long byteLength = new System.IO.FileInfo(fileName).Length;
+        fileContent = binaryReader.ReadBytes((Int32)byteLength);
+        fs.Close();
+        fs.Dispose();
+        binaryReader.Close();
+        return fileContent;
+      }
+
+      private static bool ByteArrayCompare(byte[] a1, byte[] a2)
+      {
+        if (a1.Length != a2.Length)
+          return false;
+
+        for (int i = 0; i < a1.Length; i++)
+          if (a1[i] != a2[i])
+            return false;
+
+        return true;
+      }
+
+      #region Fragment Equality
+
+      /// <summary>
+      /// Asserts that two Excels have the same content.
+      /// </summary>
+      /// <param name="expectedExcel">The expected Excel.</param>
+      /// <param name="actualExcel">The actual Excel.</param>
+      /// <exception cref="AssertionException">Thrown if the verification failed unless the current <see cref="AssertionContext.AssertionFailureBehavior" /> indicates otherwise.</exception>
+      /// <exception cref="ArgumentNullException">Thrown if <paramref name="expectedExcel"/>, <paramref name="actualExcel"/> is null.</exception>
+      public static void AreEqual(string expectedExcel, string actualExcel)
+      {
+        if (expectedExcel == null)
+          throw new ArgumentNullException("expectedExcel");
+        if (actualExcel == null)
+          throw new ArgumentNullException("actualExcel");
+
+        byte[] expectedBytes = FileToByteArray(expectedExcel);
+        byte[] actualBytes = FileToByteArray(actualExcel);
+
+        IsTrue(ByteArrayCompare(expectedBytes, actualBytes));
+      }
+
+      /// <summary>
+      /// Asserts that two Excels do not have the same content.
+      /// </summary>
+      /// <param name="expectedExcel">The expected XML fragment.</param>
+      /// <param name="actualExcel">The actual XML fragment.</param>
+      /// <exception cref="AssertionException">Thrown if the verification failed unless the current <see cref="AssertionContext.AssertionFailureBehavior" /> indicates otherwise.</exception>
+      /// <exception cref="ArgumentNullException">Thrown if <paramref name="expectedExcel"/>, <paramref name="actualExcel"/> is null.</exception>
+      public static void AreNotEqual(string expectedExcel, string actualExcel)
+      {
+        if (expectedExcel == null)
+          throw new ArgumentNullException("expectedExcel");
+        if (actualExcel == null)
+          throw new ArgumentNullException("actualExcel");
+
+        byte[] expectedBytes = FileToByteArray(expectedExcel);
+        byte[] actualBytes = FileToByteArray(actualExcel);
+
+        IsFalse(ByteArrayCompare(expectedBytes, actualBytes));
+      }
+      #endregion
+    }
+  }
+}

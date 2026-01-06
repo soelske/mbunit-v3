778f6b982507f77f33f091e32ef3bf6b20e40945
Bart Suelze <bart.suelze@i-theses.com>
2026-01-06 09:36:39 +0100

    Bumped MbUnit/Gallio to version 4.0.0.0.

--- src/Revision.cs
+++ src/Revision.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.4.11.0")]
-[assembly: AssemblyFileVersion("3.4.11.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]

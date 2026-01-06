b3b1254bd468ec7a474fb8fa8a477830e711e609
Bart Suelze <bart.suelze@i-theses.com>
2026-01-06 10:10:44 +0100

    Bumped Gallio.AutoCAD to version 4.0.0.0.

--- src/Extensions/AutoCAD/Revision.cs
+++ src/Extensions/AutoCAD/Revision.cs
@@ -2,3 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyProduct("Gallio.AutoCAD")]
+[assembly: AssemblyCopyright("Copyright © 2005-2026 Gallio Project - http://www.gallio.org/")]

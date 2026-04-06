cf7fd856812d6c0656988cd12af76cbd3ac301fd
Bart Suelze <bart.suelze@i-theses.com>
2026-04-06 09:33:05 +0200

    Revert "Bumped version to 4.0.1.0."
    This reverts commit bfcfe6a7421b993fa4926b0278ca84f69ca03e7c.

--- src/Extensions/AutoCAD/Revision.cs
+++ src/Extensions/AutoCAD/Revision.cs
@@ -1,6 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 [assembly: AssemblyProduct("Gallio.AutoCAD")]
-[assembly: AssemblyCopyright("Copyright © 2005-2026 Gallio Project - http://www.gallio.org/")]
+[assembly: AssemblyCopyright("Copyright � 2005-2026 Gallio Project - http://www.gallio.org/")]
